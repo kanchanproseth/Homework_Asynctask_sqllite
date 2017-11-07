2c50339af59dfa0e4f4d8a7629f68f135c5d3f08
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-07 10:34:33 +0700

    add auto login and using sharepreference

--- app/src/main/java/com/kanchanproseth/homework_asynctask_sqllite/controller/MainActivity.kt
+++ app/src/main/java/com/kanchanproseth/homework_asynctask_sqllite/controller/MainActivity.kt
@@ -15,6 +15,13 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import android.support.v4.content.ContextCompat.startActivity
 import com.kanchanproseth.homework_asynctask_sqllite.service.SignInService
+import android.R.id.edit
+import android.content.Context
+import android.content.SharedPreferences
+import com.kanchanproseth.homework_asynctask_sqllite.util.AESCrypt
+import android.R.id.edit
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +34,8 @@ class MainActivity : AppCompatActivity() {
         app.runConfigure()
         val usernameTextField = signInUsername.text
         val passwordTextfield = signInPassword.text
+
+
         signInButton.setOnClickListener {
             if(usernameTextField.isEmpty()){
                 Toast.makeText(this, "Please Enter Username", Toast.LENGTH_SHORT).show()
@@ -41,6 +50,18 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, Register::class.java)
             startActivity(myIntent)
         }
+
+        val pref = applicationContext.getSharedPreferences("MyPref", 0)
+            val username = pref.getString("username", "")//"No name defined" is the default value.
+            val password = pref.getString("password", "") //0 is the default value.
+            val pwDecrypt = AESCrypt.decrypt(password)
+        if (username != null && pwDecrypt != null){
+            autoSignin(username, pwDecrypt)
+        }
+
+
+
+
     }
     
 
@@ -56,17 +77,48 @@ class MainActivity : AppCompatActivity() {
                 .subscribe(
                         { result ->
                             if (result["code"] == "200"){
+                                val pref = applicationContext.getSharedPreferences("MyPref", 0)
+                                val editor = pref.edit()
+                                val pwEncrypt = AESCrypt.encrypt(password)
+                                editor.putString("username", username)
+                                editor.putString("password", pwEncrypt)
+                                editor.commit()
 
                                 val myIntent = Intent(this, Home::class.java)
-//                                myIntent.putExtra("key", value) //Optional parameters
                                 startActivity(myIntent)
+                                this.finish()
                             }else{
                                 Toast.makeText(this, result["message"], Toast.LENGTH_SHORT).show()
-//                                val myIntent = Intent(this, Register::class.java)
-////                                myIntent.putExtra("key", value) //Optional parameters
-//                                startActivity(myIntent)
+                            }
+                        },
+                        { error ->
+                            Log.e("Error", error.message)
+                        }
+                )
+    }
+
+    fun autoSignin(username: String, password: String){
+        val signinService: SignInService = app.retrofit!!.create(SignInService::class.java)
+        val model_obj = User()
+        model_obj.username = username
+        model_obj.password = password
+
+        signinService.signIn(model_obj) .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            if (result["code"] == "200"){
 
 
+                                val myIntent = Intent(this, Home::class.java)
+                                startActivity(myIntent)
+                                this.finish()
+                            }else{
+                                val pref = applicationContext.getSharedPreferences("MyPref", 0)
+                                val editor = pref.edit()
+                                editor.clear()
+                                editor.commit()
+                                Toast.makeText(this, "This Account doesn't existed anymore. please Sign in Again", Toast.LENGTH_SHORT).show()
                             }
                         },
                         { error ->
